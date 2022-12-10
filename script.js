f7f16fd6638d325ea33685efda8fd78ba0d9ec79
mohameddbenhamou <mohamedbenhamou.info@gmail.com>
2022-12-10 09:41:59 +0100

    some changes

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const body = document.getElementById("gradient");
-
+const copy = document.querySelector(".copy");
+const body = document.querySelector("#corps");
 function setGradient() {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-  css.textContent = body.style.background + ";";
+  copy.value = body.style.background;
 }
-
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
